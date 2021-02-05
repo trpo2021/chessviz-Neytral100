e1675070a502628d509cae3b11c5882f9725a564
Neytral100 <rymarev.maksim@mail.ru>
2021-02-05 23:17:55 +0700

    Add board output to file function

--- main.c
+++ main.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 
 char* board_init();
+void board_output(char*);
+char interpret(char*);
 
 int main()
 {
     char* board;
     board = board_init();
+    board_output(board);
     return 0;
 }
 
@@ -39,4 +42,76 @@ char* board_init()
     *(board + 63) = 2;
 
     return board;
+}
+
+void board_output(char* board)
+{
+    FILE* pFileBoard = fopen("Сhessboard.txt", "w");
+    if (pFileBoard == NULL) {
+        printf("Error with open the file");
+        return;
+    }
+    int i;
+    int j = 8;
+    for (i = 1; i <= 64; i++) {
+        fprintf(pFileBoard, "%c ", interpret(board + i - 1));
+        if ((i % 8) == 0) {
+            fprintf(pFileBoard, "%d ", j);
+            j--;
+        }
+        if ((i % 8) == 0)
+            fprintf(pFileBoard, "\n");
+    }
+    char c;
+    for (c = 'a'; c <= 'h'; c++)
+        fprintf(pFileBoard, "%c ", c);
+    fprintf(pFileBoard, "\n");
+
+    fclose(pFileBoard);
+}
+
+char interpret(char* figure_number)
+{
+    switch (*figure_number) {
+    case 1:
+        return 'p';
+        break;
+    case 2:
+        return 'r';
+        break;
+    case 3:
+        return 'n';
+        break;
+    case 4:
+        return 'b';
+        break;
+    case 5:
+        return 'q';
+        break;
+    case 6:
+        return 'k';
+        break;
+    case -1:
+        return 'P';
+        break;
+    case -2:
+        return 'R';
+        break;
+    case -3:
+        return 'N';
+        break;
+    case -4:
+        return 'B';
+        break;
+    case -5:
+        return 'Q';
+        break;
+    case -6:
+        return 'K';
+        break;
+    case 0:
+        return ' ';
+        break;
+    }
+    return '\0';
 }
