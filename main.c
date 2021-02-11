80d1fffbc1cb93bf5ab682d0363990024a5612dc
Neytral100 <rymarev.maksim@mail.ru>
2021-02-11 21:19:02 +0700

    Storage of board in the form of array of char

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 
 char* board_init();
 void board_output(char*);
-char interpret(char*);
 
 int main()
 {
@@ -15,31 +14,35 @@ int main()
 
 char* board_init()
 {
-    char* board = calloc(64, sizeof(int));
+    char* board = calloc(64, sizeof(char));
     char i;
 
     for (i = 8; i <= 15; i++)
-        *(board + i) = -1;
+        *(board + i) = 'P';
 
     for (i = 48; i <= 55; i++)
-        *(board + i) = 1;
+        *(board + i) = 'p';
 
-    *(board + 0) = -2;
-    *(board + 1) = -3;
-    *(board + 2) = -4;
-    *(board + 3) = -5;
-    *(board + 4) = -6;
-    *(board + 5) = -4;
-    *(board + 6) = -3;
-    *(board + 7) = -2;
-    *(board + 56) = 2;
-    *(board + 57) = 3;
-    *(board + 58) = 4;
-    *(board + 59) = 5;
-    *(board + 60) = 6;
-    *(board + 61) = 4;
-    *(board + 62) = 3;
-    *(board + 63) = 2;
+    *(board + 0) = 'R';
+    *(board + 1) = 'N';
+    *(board + 2) = 'B';
+    *(board + 3) = 'Q';
+    *(board + 4) = 'K';
+    *(board + 5) = 'B';
+    *(board + 6) = 'N';
+    *(board + 7) = 'R';
+    *(board + 56) = 'r';
+    *(board + 57) = 'n';
+    *(board + 58) = 'b';
+    *(board + 59) = 'q';
+    *(board + 60) = 'k';
+    *(board + 61) = 'b';
+    *(board + 62) = 'n';
+    *(board + 63) = 'r';
+
+    for (i = 16; i < 48; i++){
+        *(board + i) = ' ';
+    }
 
     return board;
 }
@@ -54,7 +57,7 @@ void board_output(char* board)
     int i;
     int j = 8;
     for (i = 1; i <= 64; i++) {
-        fprintf(pFileBoard, "%c ", interpret(board + i - 1));
+        fprintf(pFileBoard, "%c ", *(board + i - 1));
         if ((i % 8) == 0) {
             fprintf(pFileBoard, "%d ", j);
             j--;
@@ -68,50 +71,4 @@ void board_output(char* board)
     fprintf(pFileBoard, "\n");
 
     fclose(pFileBoard);
-}
-
-char interpret(char* figure_number)
-{
-    switch (*figure_number) {
-    case 1:
-        return 'p';
-        break;
-    case 2:
-        return 'r';
-        break;
-    case 3:
-        return 'n';
-        break;
-    case 4:
-        return 'b';
-        break;
-    case 5:
-        return 'q';
-        break;
-    case 6:
-        return 'k';
-        break;
-    case -1:
-        return 'P';
-        break;
-    case -2:
-        return 'R';
-        break;
-    case -3:
-        return 'N';
-        break;
-    case -4:
-        return 'B';
-        break;
-    case -5:
-        return 'Q';
-        break;
-    case -6:
-        return 'K';
-        break;
-    case 0:
-        return ' ';
-        break;
-    }
-    return '\0';
 }
