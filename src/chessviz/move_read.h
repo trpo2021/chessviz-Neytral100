a320471313121570035d37b4597a531c14d995e6
Neytral100 <rymarev.maksim@mail.ru>
2021-03-27 01:19:47 +0700

    Adapting reading a file

--- src/chessviz/move_read.h
+++ src/chessviz/move_read.h
@@ -1,4 +1,5 @@
+#pragma once
 #include <libchessviz/struct.h>
 
-struct List* moves_read(char* name_of_input_file);
-void read_current_move(Move* move, char* current_move);
+struct Move* moves_read(char*);
+void read_current_move(Move*, char*);
