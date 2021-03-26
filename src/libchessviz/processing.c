a51de3d4a74ec95202198961ef59d4ec53254045
Neytral100 <rymarev.maksim@mail.ru>
2021-03-27 00:27:28 +0700

    Add move caprute

--- src/libchessviz/processing.c
+++ src/libchessviz/processing.c
@@ -2,6 +2,7 @@
 #include <libchessviz/processing.h>
 #include <libchessviz/struct.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void processing(Cell* board, struct Move* move, Color color)
 {
@@ -13,12 +14,11 @@ void processing(Cell* board, struct Move* move, Color color)
 
 int checking_for_errors(Cell* board, struct Move* move, Color color)
 {
-    // checking for color
     if (selecting(board, move)->piece->color != color) {
         printf("Error, the selected piece is a different color\n");
         return -1;
     }
-    // checking for shape
+
     if (selecting(board, move)->piece->shape != move->shape) {
         printf("Error, the selected piece is a different shape\n");
         return -1;
@@ -26,26 +26,6 @@ int checking_for_errors(Cell* board, struct Move* move, Color color)
     if (move->shape == ShapePawn) {
         return pawn_check(move, color);
     }
-
-    /*
-    if (((board + (current_Move->move->start_x) * NUM_OF_ROWS
-          + (current_Move->move->start_y))
-                 ->piece->color
-         == ColorBlack)
-        && (((current_Move->move->start_y) - (current_Move->move->end_y))
-            == -1)) {
-        printf("Error, the selected Pawn can't walk this way\n");
-        return -1;
-    }
-    if (((board + (current_Move->move->start_x) * NUM_OF_ROWS
-          + (current_Move->move->start_y))
-                 ->piece->color
-         == ColorWhite)
-        && (((current_Move->move->start_y - current_Move->move->end_y) == 1))) {
-        printf("Error, the selected Pawn can't walk this way\n");
-        return -1;
-    }
-    */
     return 0;
 }
 
@@ -62,5 +42,10 @@ void action(Cell* board, struct Move* move)
         *(board + (move->end_y) * NUM_OF_ROWS + (move->end_x))
                 = *(board + (move->start_y) * NUM_OF_ROWS + (move->start_x));
         *(board + (move->start_y) * NUM_OF_ROWS + (move->start_x)) = boofer;
+    }
+    if (move->action_type == ActionTypeCapture) {
+        *(board + (move->end_y) * NUM_OF_ROWS + (move->end_x))
+                = *(board + (move->start_y) * NUM_OF_ROWS + (move->start_x));
+        (board + (move->start_y) * NUM_OF_ROWS + (move->start_x))->piece = NULL;
     }
 }
